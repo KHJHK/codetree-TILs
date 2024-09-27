c8a45cb12b7b0ce8825b00f1df280fc7186d56b0
HuiJung <86724344+KHJHK@users.noreply.github.com>
2024-09-27 22:14:33 +0900

    Title: 전투 로봇, Time: 102ms, Memory: 9MB, Status: Passed - CodeTree

--- 240927/전투 로봇/fighting-robot.java
+++ 240927/전투 로봇/fighting-robot.java
@@ -74,8 +74,15 @@ public class Main {
 		Queue<int[]> queue = new ArrayDeque<>();
 		queue.offer(new int[] {robot.r, robot.c});
 		visited[robot.r][robot.c] = true;
+		PriorityQueue<int[]> huntPQ = new PriorityQueue<>(new Comparator<int[]>() {
+			public int compare(int[] o1, int[] o2) {
+				if(o1[0] == o2[0]) return Integer.compare(o1[1], o2[1]);
+				return Integer.compare(o1[0], o2[0]);
+			}
+		});
 		int depth = 0;
 		int qSize = 1;
+		
 		
 		while(!queue.isEmpty()) {
 			depth++;
@@ -93,17 +100,24 @@ public class Main {
 					if(visited[nr][nc]) continue; //이미 지나간 길이면 continue
 					if(map[nr][nc] > robot.level) continue; //해당 칸 위에 몬스터 레벨이 더 높으면 continue
 					
-					if(map[nr][nc] != 0 && map[nr][nc] < robot.level) {
-						map[robot.r][robot.c] = 0;
-						robot.r = nr;
-						robot.c = nc;
-						map[nr][nc] = 9;
-						robot.kill();
-						return depth;
+					if(map[nr][nc] != 0 && map[nr][nc] < robot.level) { //사냥 가능할 경우 사냥 pq에 저장
+						huntPQ.offer(new int[] {nr, nc});
+						visited[nr][nc] = true;
+						continue;
 					}
 					queue.offer(new int[] {nr, nc});
 					visited[nr][nc] = true;
 				}
+			}
+			
+			if(!huntPQ.isEmpty()) { //우선순위에 따라 사냥 지정
+				int[] killLoc = huntPQ.poll();
+				map[robot.r][robot.c] = 0;
+				robot.r = killLoc[0];
+				robot.c = killLoc[1];
+				map[killLoc[0]][killLoc[1]] = 9;
+				robot.kill();
+				return depth;
 			}
 		}
 		
@@ -114,8 +128,11 @@ public class Main {
 	
 	static void printMap() {
 		for(int[] m : map) {
-			System.out.println(Arrays.toString(m));
-		}
+			for(int mm : m) {
+				if(mm == 9) System.out.printf("R%d ", robot.level);
+				else System.out.printf("%2d ", mm);
+			}System.out.println();
+		}System.out.println();
 	}
 
 }
