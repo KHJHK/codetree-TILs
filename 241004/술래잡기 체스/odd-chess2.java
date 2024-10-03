57f801326b17aa6631ffe33575dd0516c7c9aeb9
HuiJung <86724344+KHJHK@users.noreply.github.com>
2024-10-04 05:46:46 +0900

    Title: 술래잡기 체스, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241004/술래잡기 체스/odd-chess2.java
+++ 241004/술래잡기 체스/odd-chess2.java
@@ -37,9 +37,9 @@ public class Main {
 	static int maxPoint;
 	static int[] dr = {-1, -1, 0, 1, 1, 1, 0, -1};
 	static int[] dc = {0, -1, -1, -1, 0, 1, 1, 1};
-	static int[][] map = new int[4][4]; //비트마스킹 사용하기
-	static Runner[] runners = new Runner[16 + 1]; //1~16 사용, 0번은 사용 x
-	static Tag tag = new Tag();
+	static int[][] originalMap = new int[4][4]; //비트마스킹 사용하기
+	static Runner[] originalRunners = new Runner[16 + 1]; //1~16 사용, 0번은 사용 x
+	static Tag originalTag = new Tag();
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -49,79 +49,87 @@ public class Main {
 			for(int j = 0; j < 4; j++) {
 				int num = Integer.parseInt(st.nextToken());
 				int dir = Integer.parseInt(st.nextToken()) - 1;
-				runners[num] = new Runner(i, j, num, dir);
-				map[i][j] += num;
+				originalRunners[num] = new Runner(i, j, num, dir);
+				originalMap[i][j] += num;
 				if(i == 0 && j == 0) init(num, dir);
 			}
 		}
 		
-		runnerMove(maxPoint, map, runners, tag);
+		runnerMove(maxPoint, originalMap, originalRunners, originalTag);
 		System.out.println(maxPoint);
 	}
 	
 	public static void runnerMove(int point, int[][] map, Runner[] runners, Tag tag) {
+		int[][] tempMap = new int[4][];
+		for(int i = 0; i < 4; i++) tempMap[i] = Arrays.copyOf(map[i], 4);
+		Runner[] tempRunners = new Runner[17];
 		for(int i = 1; i <= 16; i++) {
-			if(runners[i].isCatched) continue;
-			int r = runners[i].r; //초기 방향
-			int c = runners[i].c; //초기 방향
-			int fd = runners[i].dir; //초기 방향
+			Runner tempRunner = runners[i];
+			tempRunners[i] = new Runner(tempRunner.r, tempRunner.c, tempRunner.num, tempRunner.dir);
+			tempRunners[i].isCatched = tempRunner.isCatched;
+		}
+		
+		for(int i = 1; i <= 16; i++) {
+			if(tempRunners[i].isCatched) continue;
+			int r = tempRunners[i].r; //초기 방향
+			int c = tempRunners[i].c; //초기 방향
+			int fd = tempRunners[i].dir; //초기 방향
 			int nr = r + dr[fd];
 			int nc = c + dc[fd];
 			
-			if(!OOB(nr, nc) && map[nr][nc] != -1) {
-				if(map[nr][nc] != 0) { //다른 말이 있는 경우
-					int temp = map[nr][nc];
-					map[nr][nc] = i;
-					map[r][c] = temp;
-					runners[i].r = nr;
-					runners[i].c = nc;
-					runners[temp].r = r;
-					runners[temp].c = c;
+			if(!OOB(nr, nc) && tempMap[nr][nc] != -1) {
+				if(tempMap[nr][nc] != 0) { //다른 말이 있는 경우
+					int temp = tempMap[nr][nc]; //해당 칸의 말 번호 저장
+					tempMap[nr][nc] = i; // 해당 칸으로 이동
+					tempMap[r][c] = temp; // 현재 칸에 이동하는 칸 말 번호 저장
+					tempRunners[i].r = nr; // 현재 말 좌표 설정
+					tempRunners[i].c = nc;
+					tempRunners[temp].r = r; //변경된 칸의 말 좌표 설정
+					tempRunners[temp].c = c;
 				}
 				
-				if(map[nr][nc] == 0){ //빈칸인 경우
-					map[nr][nc] = i;
-					map[r][c] = 0;
-					runners[i].r = nr;
-					runners[i].c = nc;
+				else if(tempMap[nr][nc] == 0){ //빈칸인 경우
+					tempMap[nr][nc] = i;
+					tempMap[r][c] = 0;
+					tempRunners[i].r = nr;
+					tempRunners[i].c = nc;
 				}
 				
 				continue;
 			}
 			
-			runners[i].changeDir();
-			while(fd != runners[i].dir) {
-				int d = runners[i].dir;
+			tempRunners[i].changeDir();
+			while(fd != tempRunners[i].dir) {
+				int d = tempRunners[i].dir;
 				nr = r + dr[d];
 				nc = c + dc[d];
 				
-				if(OOB(nr, nc) || map[nr][nc] == -1) {
-					runners[i].changeDir();
+				if(OOB(nr, nc) || tempMap[nr][nc] == -1) {
+					tempRunners[i].changeDir();
 					continue;
 				}
 				
-				if(map[nr][nc] != 0) { //다른 말이 있는 경우
-					int temp = map[nr][nc];
-					map[nr][nc] = i;
-					map[r][c] = temp;
-					runners[i].r = nr;
-					runners[i].c = nr;
-					runners[temp].r = r;
-					runners[temp].c = c;
+				if(tempMap[nr][nc] != 0) { //다른 말이 있는 경우
+					int temp = tempMap[nr][nc];
+					tempMap[nr][nc] = i;
+					tempMap[r][c] = temp;
+					tempRunners[i].r = nr;
+					tempRunners[i].c = nc;
+					tempRunners[temp].r = r;
+					tempRunners[temp].c = c;
 				}
-				
-				if(map[nr][nc] == 0){ //빈칸인 경우
-					map[nr][nc] = i;
-					map[r][c] = 0;
-					runners[i].r = nr;
-					runners[i].c = nc;
+				else if(tempMap[nr][nc] == 0){ //빈칸인 경우
+					tempMap[nr][nc] = i;
+					tempMap[r][c] = 0;
+					tempRunners[i].r = nr;
+					tempRunners[i].c = nc;
 				}
 				
 				break;
 			}
 		}
 		
-		tagMove(point, map, runners, tag);
+		tagMove(point, tempMap, tempRunners, tag);
 	}
 	
 	public static void tagMove(int point, int[][] map, Runner[] runners, Tag tag) {
@@ -157,7 +165,12 @@ public class Main {
 			
 			int nnr = nr + dr[tempTag.dir];
 			int nnc = nc + dc[tempTag.dir];
-			if(OOB(nnr, nnc) || map[nnr][nnc] == 0) maxPoint = Math.max(maxPoint, point + plusPoint); //만약 현재 이동 후 끝난다면, 포인트 갱신
+			if(OOB(nnr, nnc) || tempMap[nnr][nnc] == 0) {
+				maxPoint = Math.max(maxPoint, point + plusPoint); //만약 현재 이동 후 끝난다면, 포인트 갱신
+				nr += dr[d];
+				nc += dc[d];
+				continue;
+			}
 			else runnerMove(point + plusPoint, tempMap, tempRunners, tempTag); //끝나지 않는다면 말 움직이기
 			
 			nr += dr[d];
@@ -168,10 +181,10 @@ public class Main {
 	}
 	
 	public static void init(int num, int dir) {
-		runners[num].isCatched = true;
-		tag.dir = dir;
+		originalRunners[num].isCatched = true;
+		originalTag.dir = dir;
 		maxPoint += num;
-		map[0][0] = -1;
+		originalMap[0][0] = -1;
 	}
 	
 	public static boolean OOB(int r, int c) { return r < 0 || r >= 4 || c < 0 || c >= 4; }
